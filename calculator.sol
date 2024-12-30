953a61304fe9dadb981770dbae6121d734aba54a
Abhishek Kumar <abhishekiitkgp007@gmail.com>
2024-12-30 15:12:30 +0530

    Update calculator.sol

--- calculator.sol
+++ calculator.sol
@@ -4,42 +4,42 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract SimpleCalculator {
     
-    struct CalculationData {
+    struct Calculation_Data {
         string operation;
         int256 result;
     }
     
-    CalculationData public lastCalculation;
+    Calculation_Data public last_Calculation;
     
     function add(int256 a, int256 b) public returns (int256) {
         int256 result = a + b;
-        lastCalculation = CalculationData("Addition", result);
+        last_Calculation = Calculation_Data("Addition", result);
         return result;
     }
     
     function subtract(int256 a, int256 b) public returns (int256) {
         int256 result = a - b;
-        lastCalculation = CalculationData("Subtraction", result);
+        last_Calculation = Calculation_Data("Subtraction", result);
         return result;
     }
     
     function multiply(int256 a, int256 b) public returns (int256) {
         int256 result = a * b;
-        lastCalculation = CalculationData("Multiplication", result);
+        last_Calculation = Calculation_Data("Multiplication", result);
         return result;
     }
     
     function divide(int256 a, int256 b) public returns (int256) {
         require(b != 0, "Cannot divide by zero");
         int256 result = a / b;
-        lastCalculation = CalculationData("Division", result);
+        last_Calculation = Calculation_Data("Division", result);
         return result;
     }
     
     function remainder(int256 a, int256 b) public returns (int256) {
         require(b != 0, "Cannot divide by zero");
         int256 result = a % b;
-        lastCalculation = CalculationData("Remainder", result);
+        last_Calculation = Calculation_Data("Remainder", result);
         return result;
     }
     
@@ -48,7 +48,7 @@ contract SimpleCalculator {
         for(uint256 i = 0; i < exponent; i++) {
             result *= base;
         }
-        lastCalculation = CalculationData("Exponentiation", int256(result));
+        last_Calculation = Calculation_Data("Exponentiation", int256(result));
         return result;
     }
     
